5aaf28672936afd73d4ab574e3789ca2e9fea079
William Chan <willixmch@gmail.com>
2025-05-02 21:56:29 +0800

    Window Commit

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.shuttle"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
