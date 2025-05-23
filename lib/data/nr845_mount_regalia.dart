997370c46f4e5557c27ec4965a3048d578952f6e
William Chan <willixmch@gmail.com>
2025-05-23 18:03:06 +0800

    added nr845

---
+++ lib/data/nr845_mount_regalia.dart
@@ -0,0 +1,59 @@
+const mountRegaliaData = {
+  'estateId': 'NR845',
+  'estateTitleZh': '富豪山峰',
+  'estateTitleEn': 'Mount Regalia',
+  'routes': [
+    {
+      'routeId': 'NR845',
+      'routeNameZh': '富豪山峰至火炭站',
+      'routeNameEn': 'Mount Regalia to Fo Tan Station',
+      'infoZh': '',
+      'infoEn': '',
+      'residentFare': '0',
+      'visitorFare': 'N/A',
+      'schedules': {
+        'workday': [
+          '07:15', '07:45', '08:15', '09:15', '10:15', '12:15', '14:15', '16:15', '17:15', '18:15', '19:15', '19:45'
+        ],
+        'saturday': [
+          '07:15', '07:45', '08:15', '09:15', '10:15', '12:15', '14:15', '16:15', '17:15', '18:15', '19:15', '19:45'
+        ],
+        'sunday': [
+          '07:15', '07:45', '08:15', '09:15', '10:15', '12:15', '14:15', '16:15', '17:15', '18:15', '19:15', '19:45'
+        ],
+        'public_holiday': [
+          '07:15', '07:45', '08:15', '09:15', '10:15', '12:15', '14:15', '16:15', '17:15', '18:15', '19:15', '19:45'
+        ],
+      },
+      'stops': [
+        {
+          'stopId': 'NR845-1',
+          'stopNameZh': '富豪山峰',
+          'stopNameEn': 'Mount Regalia',
+          'etaOffset': -1,
+          'latitude': 22.411262,
+          'longitude': 114.201290,
+          'boardingStop': true,
+        },
+        {
+          'stopId': 'NR845-2',
+          'stopNameZh': '火炭站公共運輸交匯處',
+          'stopNameEn': 'Fo Tan Station Public Transport Interchange',
+          'etaOffset': 10,
+          'latitude': 22.395302,
+          'longitude': 114.197836,
+          'boardingStop': true,
+        },
+        {
+          'stopId': 'NR845-100',
+          'stopNameZh': '富豪山峰',
+          'stopNameEn': 'Mount Regalia',
+          'etaOffset': -1,
+          'latitude': 22.411262,
+          'longitude': 114.201290,
+          'boardingStop': false,
+        },
+      ],
+    },
+  ],
+};
