4af2c9b248b3dc72ad3d9df7271cb11cdacf1bdd
William Chan <willixmch@gmail.com>
2025-06-04 00:51:45 +0800

    added schedule

---
+++ lib/data/KR/kr14.dart
@@ -0,0 +1,131 @@
+const theCoronationData = {
+  'estateId': 'KR14',
+  'estateTitleZh': '御金・國峯',
+  'estateTitleEn': 'The Coronation',
+  'routes': [
+    {
+      'routeId': 'KR14-A',
+      'routeNameZh': '御金・國峯-九龍站',
+      'routeNameEn': 'The Coronation to Kowloon Station',
+      'infoZh': '',
+      'infoEn': '',
+      'residentFare': '\$0',
+      'visitorFare': '',
+      'schedules': {
+        'workday': [
+          '07:00', '07:20', '07:40', '08:00', '08:20', '08:40', '09:00', '10:20', '10:40', '17:20',
+          '17:40', '18:00', '18:20', '18:40', '19:00', '19:20', '19:40', '20:00', '20:20', '20:40'
+        ],
+        'saturday': [
+          '08:00', '08:20', '10:20', '10:40', '17:20', '17:40', '18:00', '20:00', '20:20', '20:40',
+          '21:00', '21:20', '21:40', '22:00'
+        ],
+        'sunday': [
+          '08:00', '08:20', '10:20', '10:40', '17:20', '17:40', '18:00', '20:00', '20:20', '20:40',
+          '21:00', '21:20', '21:40', '22:00'
+        ],
+        'public_holiday': [
+          '08:00', '08:20', '10:20', '10:40', '17:20', '17:40', '18:00', '20:00', '20:20', '20:40',
+          '21:00', '21:20', '21:40', '22:00'
+        ],
+      },
+      'stops': [
+        {
+          'stopId': 'KR14-1',
+          'stopNameZh': '御金・國峯',
+          'stopNameEn': 'The Coronation',
+          'etaOffset': -1,
+          'latitude': 22.3095,
+          'longitude': 114.1661,
+          'boardingStop': true,
+        },
+        {
+          'stopId': 'KR14-2',
+          'stopNameZh': '九龍站',
+          'stopNameEn': 'Kowloon Station',
+          'etaOffset': 5,
+          'latitude': 22.3052,
+          'longitude': 114.1625,
+          'boardingStop': true,
+        },
+        {
+          'stopId': 'KR14-1',
+          'stopNameZh': '御金・國峯',
+          'stopNameEn': 'The Coronation',
+          'etaOffset': -1,
+          'latitude': 22.3095,
+          'longitude': 114.1661,
+          'boardingStop': false,
+        },
+      ],
+    },
+    {
+      'routeId': 'KR14-B',
+      'routeNameZh': '御金・國峯-九龍站(特別班次)',
+      'routeNameEn': 'The Coronation to Kowloon Station (Special Services)',
+      'infoZh': '',
+      'infoEn': '',
+      'residentFare': '\$0',
+      'visitorFare': '',
+      'schedules': {
+        'workday': [
+          '09:20', '09:50', '10:20', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00',
+          '15:30', '16:00', '21:00', '21:30'
+        ],
+        'saturday': [
+          '08:20', '08:50', '09:20', '09:50', '10:20', '12:00', '12:30', '13:00', '13:30', '14:00',
+          '14:30', '15:00', '15:30', '16:00', '18:00', '18:30', '19:00', '19:30', '20:00', '22:00',
+          '22:30'
+        ],
+        'sunday': [
+          '08:20', '08:50', '09:20', '09:50', '10:20', '12:00', '12:30', '13:00', '13:30', '14:00',
+          '14:30', '15:00', '15:30', '16:00', '18:00', '18:30', '19:00', '19:30', '20:00', '22:00',
+          '22:30'
+        ],
+        'public_holiday': [
+          '08:20', '08:50', '09:20', '09:50', '10:20', '12:00', '12:30', '13:00', '13:30', '14:00',
+          '14:30', '15:00', '15:30', '16:00', '18:00', '18:30', '19:00', '19:30', '20:00', '22:00',
+          '22:30'
+        ],
+      },
+      'stops': [
+        {
+          'stopId': 'KR14-1',
+          'stopNameZh': '御金・國峯',
+          'stopNameEn': 'The Coronation',
+          'etaOffset': -1,
+          'latitude': 22.3095,
+          'longitude': 114.1661,
+          'boardingStop': true,
+        },
+        {
+          'stopId': 'KR14-3',
+          'stopNameZh': '西九龍站',
+          'stopNameEn': 'West Kowloon Station',
+          'etaOffset': 5,
+          'latitude': 22.3040,
+          'longitude': 114.1665,
+          'boardingStop': true,
+        },
+        {
+          'stopId': 'KR14-2',
+          'stopNameZh': '九龍站',
+          'stopNameEn': 'Kowloon Station',
+          'etaOffset': 11,
+          'latitude': 22.3052,
+          'longitude': 114.1625,
+          'boardingStop': true,
+        },
+        {
+          'stopId': 'KR14-1',
+          'stopNameZh': '御金・國峯',
+          'stopNameEn': 'The Coronation',
+          'etaOffset': -1,
+          'latitude': 22.3095,
+          'longitude': 114.1661,
+          'boardingStop': false,
+        },
+      ],
+    },
+  ],
+};
